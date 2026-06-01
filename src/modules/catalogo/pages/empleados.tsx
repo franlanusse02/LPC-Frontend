d5be0068703973798a37f7cc60ed5f9727701c89
Mac Fran <franlanusse02@gmail.com>
2026-06-01 11:32:14 -0300

    Added passsword update to front

--- src/modules/catalogo/pages/empleados.tsx
+++ src/modules/catalogo/pages/empleados.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft, Pencil, Plus, Trash2 } from "lucide-react";
 import { toast } from "sonner";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
